6f062d14225dc37ea4c80a54afdfc70700911cb1
Victor Marques <victormaciel28@hotmail.com>
2023-10-04 19:53:20 -0300

    Nova tentativa de ajuste do path

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/tcc-odonto/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tcc-odonto/'
+    : '/',
   plugins: [vue()],
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    outDir: 'docs',
   },
 })
